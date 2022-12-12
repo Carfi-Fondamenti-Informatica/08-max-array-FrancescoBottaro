d29238fd2ec3e294ed955c6e9a2207bbae45e84e
FrancescoBottaro <117718788+FrancescoBottaro@users.noreply.github.com>
2022-12-12 21:22:06 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+int Stampa_max (int sequenza [],int n) {
+    int max;
+
+    max = sequenza[0];
+
+    for (int c=1; c<n; c++)
+        if (sequenza[c] > max)
+        {
+            max= sequenza[c] ;
+        }
+  
+    return max;
+}
 
